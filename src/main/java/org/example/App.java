54806ffffe3ca1960ec799229c51e2e77bf159f9
Mickaël Véril <mika.veril@wanadoo.fr>
2022-07-06 15:44:38 +0200

    Update App

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.example.dao.CustomerDao;
+import org.example.entity.Customer;
+import org.example.jpa.EntityManagerSingleton;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Customer customer = new Customer();
+        customer.setFirstName("Alain");
+        customer.setLastName("Delon");
+
+        CustomerDao.create(customer);
     }
 }
